bd9123dfefb437ac7b1400fdae6b1bb8e102ab91
Adam West <Adamwest023@gmail.com>
2022-08-29 22:29:09 -0500

    created email sender

--- python_email_sender.py
+++ python_email_sender.py
@@ -1,6 +1,30 @@
 from email.message import EmailMessage
 from passwords import email_password 
+import ssl
+import smtplib
 
-email_sender = adamwest023@gmail.com
+email_sender = 'adamwest023@gmail.com'
 email_password = email_password
+ 
+ 
+email_receiver = "hejan95984@lurenwu.com"
+
+subject = "Don't forget to subscribe."
+body = """
+When you watch a video, please be sure to subscribe
+"""
+
+em = EmailMessage()
+em["From"] = email_sender
+em["To"] = email_receiver
+em["subject"] = subject
+em.set_content(body)
+
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com",465, context = context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver,em.as_string())
+        
  
