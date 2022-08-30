7537a411cffadd98c38d7047c0f574ea95174552
Adam West <Adamwest023@gmail.com>
2022-08-29 21:51:43 -0500

    created base file for email sender python tutorial

---
+++ python_email_sender.py
@@ -0,0 +1,6 @@
+from email.message import EmailMessage
+from passwords import email_password 
+
+email_sender = adamwest023@gmail.com
+email_password = email_password
+ 
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-print('hello world')
-print('hello world')
-print('hello world')
-print('hello world')
+print("Hello, world!")
+print(len("fdsfsgfsgfsgfd"))
+
+print(max(40,12,444))
