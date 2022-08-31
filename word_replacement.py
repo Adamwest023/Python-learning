dde01f4d2e0dc2a6a212ba43a2418939fd7fdc0d
Adam West <Adamwest023@gmail.com>
2022-08-30 23:38:50 -0500

    created basic word replacement

---
+++ word_replacement.py
@@ -0,0 +1,8 @@
+def replace_word():
+
+    str = "Hi guys, I am Adam. Hi Hi Hi Hi"
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Enter the replacement word: ")
+    print(str.replace(word_to_replace, word_replacement))
+    
+replace_word()
