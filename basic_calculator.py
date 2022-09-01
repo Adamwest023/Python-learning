39c0b8452b7606a0c795dc6baf2943c0da8aa8e4
Adam West <Adamwest023@gmail.com>
2022-09-01 12:42:13 -0500

    added sub,div,multiply functions

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,9 +1,21 @@
-# define functions needed: add,sub,div,mul
+# define functions needed: add,sub,div,mult
 # print options to the user
 # ask for values
 # call the functions
 # while loop to continue the program until the user exits
 
 def add(a, b):
-    answer = a+b
+    answer = a + b
     print(str(a) + " + " + str(b) + " = " + str(answer))
+
+def sub(a, b):
+    answer = a - b 
+    print(str(a) + " - " + str(b) + " = " + str(answer))
+    
+def div(a, b):
+    answer = a / b 
+    print(str(a) + " / " + str(b) + " = " + str(answer))
+    
+def mult(a, b):
+    answer = a * b 
+    print(str(a) + " * " + str(b) + " = " + str(answer))
