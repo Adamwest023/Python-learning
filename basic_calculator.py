40d6b740dd384958e82a5312e18f418f5588bd4b
Adam West <Adamwest023@gmail.com>
2022-09-01 12:38:48 -0500

    created addition function in basic calculator

---
+++ basic_calculator.py
@@ -0,0 +1,9 @@
+# define functions needed: add,sub,div,mul
+# print options to the user
+# ask for values
+# call the functions
+# while loop to continue the program until the user exits
+
+def add(a, b):
+    answer = a+b
+    print(str(a) + " + " + str(b) + " = " + str(answer))
