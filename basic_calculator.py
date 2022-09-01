fbdbd3bdada31b62a7a20b2da8dd92ed3ef9b498
Adam West <Adamwest023@gmail.com>
2022-09-01 12:49:43 -0500

    created first choice for user

--- basic_calculator.py
+++ basic_calculator.py
@@ -19,3 +19,21 @@ def div(a, b):
 def mult(a, b):
     answer = a * b 
     print(str(a) + " * " + str(b) + " = " + str(answer))
+    
+
+print("A. Addition")
+print("B. Subtraction")
+print("C. Multiplication")
+print("D. Division")
+print("E. Exit")
+
+choice = input("input your choice")
+
+if choice == "a" or choice =="A":
+    print('Addition')
+    a = int(input("input first number: "))
+    b = int(input("input second number: "))
+    add(a,b)
+elif choice == "b" or choice == "B": 
+    print('Subtraction')
+      
