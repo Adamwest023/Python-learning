2dc8bff89d88f7fcfc8a9060fc6eb916c1fdf86e
Adam West <Adamwest023@gmail.com>
2022-09-02 13:52:51 -0500

    added quesitons for quiz

---
+++ quiz_program.py
@@ -0,0 +1,39 @@
+# a dictionary that stores questions and answers
+# have a variable that tracks the score
+# loop through the dictionary using the key value pairs
+# display each question to the user and allow them to answer
+# tell them if they are correct
+# show the final result when the quiz is finished
+
+quiz = {
+    "question1": {
+        "question": "What is the capital of France?",
+        "answer": "Paris"
+    },
+    "question2": {
+        "question": "What is the capital of Germany?",
+        "answer": "Berlin"
+    },
+    "question3": {
+        "question": "What is the capital of Italy",
+        "answer": "Rome"
+    },
+    "question4": {
+        "question": "What is the capital of Spain?",
+        "answer": "Madrid"
+    },
+    "question5": {
+        "question": "What is the capital of Portugal",
+        "answer": "Lisbon"
+    },
+    "question6": {
+        "question": "What is the capital of Switzerland?",
+        "answer": "Bern"
+    },
+    "question7": {
+        "question": "What is the capital of Austria?",
+        "answer": "Vienna"
+    },
+}
+
+score = 0 
