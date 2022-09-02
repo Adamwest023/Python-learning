393ac1a219b53ebc32fd7206576f498a7af54b0d
Adam West <Adamwest023@gmail.com>
2022-09-02 15:29:56 -0500

    created dice roll simulator

---
+++ generators/dice_rolling_simulator.py
@@ -0,0 +1,68 @@
+# import random
+# define function to roll dice
+# create a dictionary that will hold the drawings of the dice
+# while loop to reroll dice 
+
+import random
+
+def roll_dice():
+    dice_drawing = {
+        1: (
+            " ----- ",
+            "|     |",
+            "|  o  |",
+            "|     |",
+            " ----- ",
+        ),
+        2: (
+            " ----- ",
+            "|o    |",
+            "|  2  |",
+            "|    o|",
+            " ----- ",
+        ),
+        3: (
+            " ----- ",
+            "|o   3|",
+            "|  o  |",
+            "|3   o|",
+            " ----- ",
+        ),
+        4: (
+            " ----- ",
+            "|o   o|",
+            "|  4  |",
+            "|o   o|",
+            " ----- ",
+        ),
+        5: (
+            " ----- ",
+            "|o   o|",
+            "|  o  |",
+            "|o   o|",
+            " ----- ",
+        ),
+        6: (
+            " ----- ",
+            "|o   o|",
+            "|o 6 o|",
+            "|o   o|",
+            " ----- ",
+        ),
+
+    }
+    
+    roll = input("Roll the dice? (Yes/No): ")
+    
+    while roll.lower() == "Yes".lower():
+        dice1 = random.randint(1,6)
+        dice2 = random.randint(1,6)
+        
+        print("dice rolled: {} and {}".format(dice1, dice2))
+        print("\n".join(dice_drawing[dice1]))
+        print("\n".join(dice_drawing[dice2]))
+        
+        
+        roll = input("Roll again? (Yes/No): ")
+        
+roll_dice()
