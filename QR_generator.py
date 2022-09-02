68c1c2a0984d08b15a6b57ffbf691a208da071e5
Adam West <Adamwest023@gmail.com>
2022-09-02 14:16:59 -0500

    created qe code generator

---
+++ QR_generator.py
@@ -0,0 +1,25 @@
+# install all the libraries needed
+# create a function that collections a text and converts it to a QR Code
+# save the code as an image
+# run the function
+from turtle import fillcolor
+import qrcode
+
+
+def generate_qrcode(text):
+
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fillcolor="black", back_color="white")
+    img.save("qrimg001.png")
+    
+    
+url = input("Enter your url: ")
+generate_qrcode(url)
