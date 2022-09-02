f18b0025fab4f3f95d1b9e0bb46f5f2948ac7fb1
Adam West <Adamwest023@gmail.com>
2022-09-02 14:02:53 -0500

    completed quiz

--- quiz_program.py
+++ quiz_program.py
@@ -37,3 +37,23 @@ quiz = {
 }
 
 score = 0 
+
+for key, value in quiz.items():
+    print(value['question'])
+    answer = input("Answer? ")
+    
+    if answer.lower() == value['answer'].lower():
+        print('Correct')
+        score = score + 1
+        print("Your score is: " + str(score))
+        print("")
+        
+    else:
+        print("Wrong answer!")
+        print("The correct answer is: " + value['answer'])
+        print("Your score is " + str(score))
+        print("")
+        
+
+print("You got " + str(score) + " out of  7 questions correctly")
+print("Your percentage is  " + str(int(score/7 *100)) + "%")
