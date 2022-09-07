32ec23a48cb620ad26b07555d3e6a5cd915231af
Adam West <Adamwest023@gmail.com>
2022-09-07 14:52:53 -0500

    created a text message automation and scheduler?

--- automation/python_automation.py
+++ automation/python_automation.py
@@ -1,6 +1,21 @@
 from credentials import phone_number
  
 import requests
+import schedule
+import time
 
 def send_message():
-    resp = requests.post('http://')
+    resp = requests.post('https://textbelt.com/text', {
+  'phone': phone_number,
+  'message': 'Hey don\'t forget to pray',
+  'key': 'textbelt',
+})
+    print(resp.json())
+    
+schedule.every().day.at("14:50").do(send_message)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
