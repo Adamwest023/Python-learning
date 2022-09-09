06c0f2ba89511f0bcfac4a887766022ee85ff4de
Adam West <Adamwest023@gmail.com>
2022-09-09 16:21:45 -0500

    completed python lessons

--- calculators/graph_plotter.py
+++ calculators/graph_plotter.py
@@ -3,11 +3,16 @@ import matplotlib.pyplot as plt
 x = [2, 4, 5, 6]
 y = [2, 3, 6, 7]
 
-plt.plot(x, y)
+plt.plot(x, y,color="green",linestyle = "dashed",linewidth = 3,marker='o', markerfacecolor = 'blue',markersize = 12,label="Line 1")
+
+
+plt.xlim(1,8)
+plt.ylim(1,8)
+
 
 plt.xlabel("X axis")
 plt.ylabel("Y axis")
 
-plt.title("Demo graph")
+plt.title("Demo graph - Customization")
 
 plt.show()
